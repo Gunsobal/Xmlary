9a3dfea44df4717e4ba9fcad69d6e5096546771f
Gunnar Baldursson <Gunso_Baldurs@hotmail.com>
2017-09-03 10:45:57 +0000

    changed a comment

--- src/XmlMessage.php
+++ src/XmlMessage.php
@@ -28,10 +28,10 @@ class XmlMessage
     /** @var string $_name The name of the root element in the xml message, defaults to class name **/
     protected $_name;
 
-    /** @var string $_build The name of the derivative class' build method which returns an array for xmlify, defaults to build **/
+    /** @var string $_build The name of the class' build method which returns an array for xmlify, defaults to build **/
     protected $_build;
 
-    /** @var array $_required Overwrite in derivative class in a way that each key in $_required is a required field in the message, with its value being a custom error message **/
+    /** @var array $_required Each key is a required field in the message, with its value being a custom error message **/
     protected $_required = [];
 
     public function __construct($arr = []){
